a48223526f3b2b9255e71631432f8ef48d7e0fd2
pesta <pesta.michal29@gmail.com>
2024-03-25 16:50:10 +0100

    finished

--- algorithms.py
+++ algorithms.py
@@ -16,29 +16,6 @@ class PriorityQueue:
     def get(self):
         return heapq.heappop(self.list)[1]
 
-def recreate_path(start, goal, path):
-    path_info = []
-    current_stop = goal
-    end_stop = goal
-    current_line = path[goal][1].line
-    arrival = path[goal][1].arrival
-
-
-    while current_stop != start:
-        edge = path[current_stop][1]
-        current_stop = path[current_stop][0]
-        if edge.line != current_line or current_stop == start:
-            path_info.append(
-                f'Line: {current_line} dep: {current_stop} at '
-                f'{edge.departure} arr: {end_stop} at {arrival}'
-            )
-            end_stop = current_stop
-            arrival = edge.arrival
-            current_line = edge.line
-
-    path_info.reverse()
-    for line in path_info:
-        print(line)
 
 def recreate_path2(start, goal, path):
     path_info = []
@@ -63,15 +40,13 @@ def recreate_path2(start, goal, path):
     path_info.reverse()
     for line in path_info:
         print(line)
-
+    return len(path_info) - 1
 
 def dijkstra_time(graph, start, goal, start_time):
     q = PriorityQueue()
     q.put(start, 0)
-    current_cost = dict()
-    path = dict()
-    current_cost[start] = 0
-    path[start] = None
+    current_cost = {start: 0}
+    path = {start: None}
 
     t1 = time.time()
 
@@ -79,8 +54,10 @@ def dijkstra_time(graph, start, goal, start_time):
         current_node = q.get()
         current_time = start_time + current_cost[current_node]
         if current_node == goal:
-            recreate_path2(start, goal, path)
-            print(f'Dijsktra time: {time.time() - t1}')
+            transfers = recreate_path2(start, goal, path)
+            print(f'Dijsktra calculation time: {time.time() - t1}')
+            print(f'Dijkstra total travel time: {(path[goal][1].arrival - start_time).seconds()}')
+            print(f'Dijkstra total transfers: {transfers}')
             return
 
         for node in graph.get_neighbors(current_node):
@@ -95,16 +72,15 @@ def dijkstra_time(graph, start, goal, start_time):
                 priority = cost
                 q.put(next_node, priority)
                 path[next_node] = (current_node, edge)
+    print('Path not found')
     return None
 
 
 def astar_time(graph, start, goal, start_time):
     q = PriorityQueue()
     q.put(start, 0)
-    current_cost = dict()
-    path = dict()
-    current_cost[start] = 0
-    path[start] = None
+    current_cost = {start: 0}
+    path = {start: None}
 
     t1 = time.time()
 
@@ -112,8 +88,10 @@ def astar_time(graph, start, goal, start_time):
         current_node = q.get()
         current_time = start_time + current_cost[current_node]
         if current_node == goal:
-            recreate_path2(start, goal, path)
-            print(f'A* time: {time.time() - t1}')
+            transfers = recreate_path2(start, goal, path)
+            print(f'A* calculation time: {time.time() - t1}')
+            print(f'A* total travel time: {(path[goal][1].arrival - start_time).seconds()}')
+            print(f'A* total transfers: {transfers}')
             return
 
         for node in graph.get_neighbors(current_node):
@@ -128,7 +106,7 @@ def astar_time(graph, start, goal, start_time):
                 priority = cost + time_heuristic((graph.nodes[next_node]), (graph.nodes[goal]))
                 q.put(next_node, priority)
                 path[next_node] = (current_node, edge)
-
+    print('Path not found')
     return None
 
 
@@ -140,34 +118,43 @@ def time_heuristic(current_node, goal):
 def astar_transfers(graph, start, goal, start_time):
     q = PriorityQueue()
     q.put(start, 0)
-    current_cost = dict()
-    path = dict()
-    current_cost[start] = 0
-    path[start] = None
+    current_cost = {start: 0}
+    path = {start: None}
+    node_time = {start: 0}
+
+    goal_lines = []
+    for edge in graph.graph[goal]:
+        goal_lines.append(edge.line)
 
     t1 = time.time()
 
     while not q.empty():
         current_node = q.get()
-        current_time = start_time + current_cost[current_node]
+        current_time = start_time + node_time[current_node]
         if current_node == goal:
-            recreate_path2(start, goal, path)
-            print(f'A* time: {time.time() - t1}')
+            transfers = recreate_path2(start, goal, path)
+            print(f'A* transfers calculation time: {time.time() - t1}')
+            print(f'A* transfers total travel time: {(path[goal][1].arrival - start_time).seconds()}')
+            print(f'A* total transfers: {transfers}')
             return
 
-        for node in graph.get_neighbors(current_node):
-            edge = graph.get_fastest_connection(current_node, node, current_time)
-            if edge is None:
-                continue
-            cost = current_cost[current_node] + (edge.arrival - current_time).seconds()
+        for edge in graph.get_connections_after_time(current_node, current_time):
+
             next_node = edge.end_stop_name
+
+            new_time = node_time[current_node] + (edge.arrival - current_time).seconds()
+            cost = current_cost[current_node] + (0 if path[current_node] is None or edge.line == path[current_node][1].line else 2) + new_time / 3600
 
             if next_node not in current_cost or cost < current_cost[next_node]:
                 current_cost[next_node] = cost
-                priority = cost + time_heuristic((graph.nodes[next_node]), (graph.nodes[goal]))
+                node_time[next_node] = new_time
+                priority = cost + transfer_heuristic(edge.line, goal_lines)
                 q.put(next_node, priority)
                 path[next_node] = (current_node, edge)
-
+    print('Path not found')
     return None
 
+
+def transfer_heuristic(current_line, goal_lines):
+    return 0 if current_line in goal_lines else 1
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import pickle
+import random
 
 from algorithms import *
 from graph import Graph
@@ -32,17 +33,90 @@ def restore_graph():
     # print(graph.get_neighbors('Miękinia - Urząd Gminy (51.19386776, 16.73806496)'))
     # print(graph.get_fastest_connection('Miękinia - Urząd Gminy (51.19386776, 16.73806496)', 'Klęka (51.20861669, 16.7499646)', '12:00:00'))
 
+def get_random_nodes(n):
+    start_nodes = []
+    end_nodes = []
+    start_times = []
+    for i in range(n):
+        start_nodes.append(random.choice(list(graph.nodes.keys())))
+        end_nodes.append(random.choice(list(graph.nodes.keys())))
+        start_times.append(BusTime(h=random.randint(1, 22), m=random.randint(0, 59), s=random.randint(0, 59)))
+    return start_nodes, end_nodes, start_times
+
+def get_example_nodes():
+    return \
+        ['Strzegomska (krzyżówka) (51.112407, 16.960639)', 'Hutmen (51.09602766, 16.98631833)', 'RUBCZAKA (Stacja kolejowa) (51.14330576, 16.86637818)', 'Polkowicka (51.15526292, 16.87306982)', 'Dzielna (51.13863431, 16.97689014)', 'Żmudzka (51.145381, 17.141698)', 'SĘPOLNO (51.11377011, 17.10379057)', 'Jasińskiej (51.12098931, 16.87663189)', 'Brodzka (51.15816026, 16.9223663)', 'MUCHOBÓR MAŁY (Stacja kolejowa) (51.11257866, 16.9706042)', 'Stępin (51.20848301, 17.28088818)', 'Mokronos Dolny - Stawowa/Szczęśliwa (51.07132146, 16.93312583)', 'Karwińska (51.08204577, 17.0769832)'], \
+        ['Wiaduktowa (51.07545298, 17.07609286)', 'Weigla (Szpital) (51.07644889, 17.01767649)', 'Gałczyńskiego (51.05505442, 17.03221101)', 'Dworska (51.14360635, 16.95344812)', 'Zalewowa (51.09049584, 17.13507911)', 'Lutynia I (51.135712, 16.771781)', 'Stanisławowska (W.K. Formaty) (51.09694613, 16.94689792)', 'Zielińskiego (51.098522, 17.02050515)', 'ROD Zgoda (51.06605814, 17.05995315)', 'Chińska (51.06088513, 17.08175746)', 'Ferma - stadnina koni (51.1180276, 16.8253587)', 'Psie Pole (Rondo Lotników Polskich) (51.14780568, 17.1149333)', 'DWORZEC AUTOBUSOWY (51.09746415, 17.03277662)'], \
+        ['20:43:41', '10:48:57', '15:06:51', '19:45:57', '07:18:15', '15:45:00', '19:19:33', '10:44:59', '03:25:22', '14:16:09', '09:55:21', '06:08:50', '05:38:22']
+
 
 if __name__ == '__main__':
-    # create_graph_from_a_file()
+
+
+    # time = BusTime(time_str='16:58:00')
+
+    # dijkstra_time(graph, 'Wyszyńskiego (51.12232447, 17.05196291)', 'Jedności Narodowej (51.12102, 17.04326)', time)
+    # astar_time(graph, 'Babimojska (51.114068, 16.977603)', 'Biegasa (51.099809, 17.086666)', time)
+    # astar_transfers(graph, 'Babimojska (51.114068, 16.977603)', 'Biegasa (51.099809, 17.086666)', time)
 
     graph = restore_graph()
-    print(len(graph.graph))
-    time = BusTime(time_str='12:22:00')
-    # result = graph.get_connections_after_time('Babimojska (51.114068, 16.977603)', time)
-    # for edge in result:
-    #     print(edge)
-    dijkstra_time(graph, 'Wyszyńskiego (51.12232447, 17.05196291)', 'Jedności Narodowej (51.12102, 17.04326)', time)
-    # astar_time(graph, 'Babimojska (51.114068, 16.977603)', 'Biegasa (51.099809, 17.086666)', time)
+    #n = 3
+    starting_nodes, end_nodes, times = get_example_nodes()
+    n = len(starting_nodes)
+        #get_random_nodes(n)
+
+    for i in range(n):
+        print(f'{starting_nodes[i]} ==> {end_nodes[i]} at: {times[i]}')
+        print()
+        dijkstra_time(graph, starting_nodes[i], end_nodes[i], BusTime(time_str=times[i]))
+        print()
+        astar_time(graph, starting_nodes[i], end_nodes[i], BusTime(time_str=times[i]))
+        print()
+        astar_transfers(graph, starting_nodes[i], end_nodes[i], BusTime(time_str=times[i]))
+        print('=====================')
+
+    #
+    # while True:
+    #     print('Select an action:')
+    #     print('[*] Create new graph - 1')
+    #     print('[*] Restore existing graph - 2')
+    #     option = input()
+    #     if int(option) == 1:
+    #         create_graph_from_a_file()
+    #         break
+    #     elif int(option) == 2:
+    #         print('Restoring graph from the local file...')
+    #         graph = restore_graph()
+    #         break
+    #
+    # print('Graph restored successfully!')
+    # while True:
+    #     start_stop = input('Departure stop: ')
+    #     end_stop = input('Destination: ')
+    #     print('Choose criteria: ')
+    #     print('[*] Time criteria - t')
+    #     print('[*] Transfers criteria - p')
+    #     criteria = input()
+    #     start_time = input('Departure time: ')
+    #     if str(criteria) == 't':
+    #         try:
+    #             astar_time(graph, start_stop, end_stop, BusTime(time_str=start_time))
+    #         except:
+    #             print('An error occured')
+    #     elif str(criteria) == 'p':
+    #         try:
+    #             astar_transfers(graph, start_stop, end_stop, BusTime(time_str=start_time))
+    #         except:
+    #             print('An error occured')
+    #
+    #     print('Exit program? [Y/n]')
+    #     exit_program = input()
+    #     if exit_program == '' or exit_program == 'y' or exit_program == 'Y':
+    #         print('Exiting...')
+    #         break
+
+
+
+
 
 
